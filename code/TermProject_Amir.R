1915169abe2688c7ee2cab165e6be3e9b7e1617c
Amirsaman <ashtarikntu@gmail.com>
2018-05-25 22:32:05 +0900

    prepared data

--- code/TermProject_Amir.R
+++ code/TermProject_Amir.R
@@ -1,7 +1,7 @@
 dirname(rstudioapi::getSourceEditorContext()$path)
 setwd("/Users/Amirsam/Documents/GitHub/")
 getwd()
-rm(list=ls())
+#rm(list=ls())
 
 
 
@@ -12,6 +12,7 @@ library(dplyr)
 library(readr)
 #install.packages("corrplot")
 library(corrplot)
+library(lattice)
 library(caret)
 #install.packages("ROCR")
 library(gplots)
@@ -19,11 +20,136 @@ library(ROCR)
 library(tree)
 library(randomForest)
 #install.packages("rstanarm")
+#install.packages("ggridges")
 library(Rcpp)
 library(rstanarm)
 library(pROC)
 
-pima <- read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data",col_names=c("Pregnant","Plasma_Glucose","Dias_BP","Triceps_Skin","Serum_Insulin","BMI","DPF","Age","Diabetes"))
+
+library(data.table)
+install.packages("R.utils")
+library(R.oo)
+library(R.methodsS3)
+library(R.utils) 
+install.packages("downloader")
+library(downloader)
+library(lubridate)
+library(plyr)
+library(stats)
+library(graphics)
+library(lattice)
+library(ggplot2)
+
+
+if(!file.exists("repdata-data-StormData.csv.bz2")) {
+  download.file("https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2", destfile = "repdata-data-StormData.csv.bz2", method = "curl")
+}
+
+url <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
+bz2.data <- "repdata_data_StormData.csv.bz2"
+download(url, bz2.data, mode = "wb")  
+
+bunzip2(bz2.data, "repdata-data-StormData.csv", overwrite=TRUE, remove = FALSE)
+
+storm <- read.table("repdata-data-StormData.csv", sep = ",", header = TRUE)
+
+
+storm1 = storm
+
+
+
+storm1$Begin.Time <- as.POSIXct(strptime(storm1$BGN_DATE, format ="%m/%d/%Y %H:%M:%S"))
+
+##storm1$End.Time <- as.POSIXct(strptime(storm1$END_DATE, format="%m/%d/%Y %H:%M:%S"))
+
+storm1$Year.Begin <- year(as.Date(storm1$Begin.Time))
+
+
+
+## Changing to AVALANCHE
+storm1 <- transform(storm1,
+                    EVTYPE = revalue(storm1$EVTYPE, c("AVALANCE" = "Avalanche", "AVALANCHE" = "AVALANCHE")))
+
+## Changing to Thunderstorm Wind"
+
+storm1 <- transform(storm1,
+                    EVTYPE = revalue(storm1$EVTYPE, c("TSTM WIND" = "THUNDERSTORM WIND")))
+
+## Changing to Rip Current
+
+storm1 <- transform(storm1,
+                    EVTYPE = revalue(storm1$EVTYPE, c("RIP CURRENTS" = "RIP CURRENT")))
+
+## Changing to Extreme Cold/Wind Chill
+
+storm1 <- transform(storm1,
+                    EVTYPE = revalue(storm1$EVTYPE, c("EXTREME COLD" = "EXTREME COLD/WIND CHILL", "EXTREME WINDCHILL" = "EXTREME COLD/WIND CHILL")))
+
+## Changing to Excessive Heat
+
+storm1 <- transform(storm1,
+                    EVTYPE = revalue(storm1$EVTYPE, c("EXTREME HEAT" = "EXCESSIVE HEAT", "RECORD/EXCESSIVE HEAT" = "EXCESSIVE HEAT")))
+
+## Changing to Hurricane/Typhoon cathegory
+
+storm1 <- transform(storm1,
+                    EVTYPE = revalue(storm1$EVTYPE, c("HURRICANE" = "HURRICANE/TYPHOON")))
+
+## Changing to High Surf
+
+storm1 <- transform(storm1,
+                    EVTYPE = revalue(storm1$EVTYPE, c("HEAVY SURF/HIGH SURF" = "HIGH SURF")))
+
+## Changing to the new name "Debris Flow"
+
+storm1 <- transform(storm1,
+                    EVTYPE = revalue(storm1$EVTYPE, c("LANDSLIDE" = "DEBRIS FLOW")))
+
+## Changing to Cold/Wind Chill cathegory
+
+storm1 <- transform(storm1,
+                    EVTYPE = revalue(storm1$EVTYPE, c("COLD" = "COLD/WIND CHILL")))
+
+## Changing to High Wind
+
+storm1 <- transform(storm1,
+                    EVTYPE = revalue(storm1$EVTYPE, c("HIGH WINDS" = "HIGH WIND")))
+
+## Changing to Heat
+
+storm1 <- transform(storm1,
+                    EVTYPE = revalue(storm1$EVTYPE, c("UNSEASONABLY WARM AND DRY" = "HEAT")))
+
+## Changing to Heavy Rain, according to the document
+
+storm1 <- transform(storm1,
+                    EVTYPE = revalue(storm1$EVTYPE, c("URBAN/SML STREAM FLD" = "HEAVY RAIN")))
+
+## Changing to Winter Weather
+
+storm1 <- transform(storm1,
+                    EVTYPE = revalue(storm1$EVTYPE, c("WINTER WEATHER/MIX" = "WINTER WEATHER")))
+
+
+
+## storm2 <- data.frame(storm1$EVTYPE, storm1$FATALITIES, storm1$INJURIES, storm1$PROPDMG, storm1$PROPDMGEXP, storm1$CROPDMG, storm1$CROPDMGEXP, storm1$Year.Begin)
+## colnames(storm2) <- c("Event", "Fatalities", "Injuries", "PROPDMG", "PROPDMGEXP", "CROPDMG", "CROPDMGEXP", "Year" )
+
+storm2 <- storm1
+
+head(storm2)
+
+
+
+
+
+
+pima = read_csv("WeatherEvents.csv",)
+str(pima)
+summary(pima)
+head(pima,2)
+
+colnames(pima) = c("Pregnant","Plasma_Glucose","Dias_BP","Triceps_Skin","Serum_Insulin","BMI","DPF","Age","Diabetes")
 
 #Lets count the no. of NA values
 sapply(pima, function(x) sum(is.na(x)))
