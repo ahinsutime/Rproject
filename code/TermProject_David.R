661833560f823bde2614cba06c4682f1eadaa770
eli-kaist <lelias@kaist.ac.kr>
2018-05-28 18:01:07 +0900

    new code
    new code

--- code/TermProject_David.R
+++ code/TermProject_David.R
@@ -1,25 +1,24 @@
 rm(list=ls())
 dev.off()
-
 setwd("C:/Rdata")
 
 #install.packages("githubinstall")
 #library(githubinstall)
 #githubinstall("https://github.com/awesomedata/awesome-public-datasets")
 
-
 ############1.Preparing data###################
 #**(1)Install and load packages----------------
 #install & load
+#if you have already installed the packages only run line 12 and skip up to line 20
+packages <- c("R.utils", "data.table", "downloader", "lubridate", "plyr","dplyr")
 ipak <- function(pkg){
   new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
   if (length(new.pkg)) 
     install.packages(new.pkg, dependencies = TRUE)
   sapply(pkg, require, character.only = TRUE)
 }
-packages <- c("R.utils", "data.table", "downloader", "lubridate", "plyr")
 ipak(packages)
-library(dplyr)
+lapply(packages, require,character.only=T)
 #**(2)Download data----------------
 #fread is much much much faster than read. Let's use fread to read csv datasets
 if(!file.exists("repdata-data-StormData.csv.bz2")) {
@@ -33,12 +32,12 @@ storm <- fread("repdata-data-StormData.csv", sep = ",", header = TRUE)
 storm1 = storm
 #**(3)Process data----------------
 #subset relevant columns
-#remove state data, remarks, refnum
+#remove county data, remarks, refnum
 storm2 <- storm1[,-c(5:6,14:15,31,36:37)]
 #**(5)change "", "+","-","?" to NA----------------
 storm2[ storm2 == "" ] <- NA
 storm3=storm2
-storm3[ storm3 == "+" ] <- NA
+storm3[ storm3 == "+" ] <- "O"
 storm4=storm3
 storm4[ storm4 == "-" ] <- NA
 storm5=storm4
@@ -49,18 +48,17 @@ na_count3 <-sapply(storm4, function(y) sum(length(which(is.na(y)))))
 na_count4 <-sapply(storm5, function(y) sum(length(which(is.na(y)))))
 na_count_tab <- data.frame(na_count,na_count2,na_count3,na_count4)
 na_count_tab
-
 ############2.Preparing data###################
 ##resources:
 #http://rstudio-pubs-static.s3.amazonaws.com/25710_7f98ce257ba941a3a9bd5e73186dc730.html
+#https://rstudio-pubs-static.s3.amazonaws.com/58957_37b6723ee52b455990e149edde45e5b6.html
 #**(1)Basic statistics
 storm5
-
 #**(2)Which events where the most fatals
 storm5<-transform(storm5, EVTYPE = factor(EVTYPE)) #EVTYPE as factors
-storm5$EVTYPE <- toupper(storm5$EVTYPE)
+storm5$EVTYPE <- toupper(storm5$EVTYPE) #upper case all characters for consistency
 unique(storm5$EVTYPE)#total of 898 events
-#Group event types by contents
+#Group event types by contents containing keywords
 storm5$EVTYPE[grep("*TORNADO*", storm5$EVTYPE)] <- "TORNADO"
 storm5$EVTYPE[grep("*HURRICANE*|*TYPHOON*", storm5$EVTYPE)] <- "HURRICANE"
 storm5$EVTYPE[grep("*WIND*", storm5$EVTYPE)] <- "WIND"
@@ -75,7 +73,33 @@ storm5$EVTYPE[grep("*FOG*", storm5$EVTYPE)] <- "FOG"
 storm5$EVTYPE[grep("*DRY*|*DROUGHT*", storm5$EVTYPE)] <- "DRY"
 storm5$EVTYPE[grep("*LANDSLIDE*|*LAND*|*AVALANCHE*|*SLIDE*", storm5$EVTYPE)] <- "LANDSLIDE"
 storm5$EVTYPE[grep("*ICE*|*ICY*|*FROST*", storm5$EVTYPE)] <- "ICE"
-
+#**(3)Unite propdmg with propdmgexp and corpdm with cropdmgexp
+unique(storm5$CROPDMGEXP) #check unique values
+unique(storm5$PROPDMGEXP) #check unique values
+#**(4)transform characters to numeric values
+#first, transform all characters to uppercase for consistency
+storm5$CROPDMGEXP <- toupper(storm5$CROPDMGEXP)
+storm5$PROPDMGEXP <- toupper(storm5$PROPDMGEXP)
+#second, unite columns
+storm6 =storm5
+unique(storm6$PROPDMGEXP)
+storm6 <- transform(storm6,
+                    PROPDMGEXP = revalue(storm6$PROPDMGEXP, c("0" = "T","1" = "T", "2" = "T", "3" = "T", "4" = "T", "5" = "T", "6" = "T", "7" ="T", "8"="T")))
+storm6$PROPDMG_t <- ifelse(storm6$PROPDMGEXP == "T", 1e1*storm6$PROPDMG,
+                           ifelse(storm6$PROPDMGEXP== "H", 1e2*storm6$PROPDMG,
+                                  ifelse(storm6$PROPDMGEXP == "K", 1e3*storm6$PROPDMG,
+                                         ifelse(storm6$PROPDMGEXP == "M", 1e6*storm6$PROPDMG,
+                                                ifelse(storm6$PROPDMGEXP == "B", 1e9*storm6$PROPDMG,
+                                                       ifelse(storm6$PROPDMGEXP == "O", 1*storm6$PROPDMG,0))))))
+unique(storm6$CROPDMGEXP)
+storm6 <- transform(storm6,
+                    CROPDMGEXP = revalue(storm6$CROPDMGEXP, c("0" = "T", "2" = "T")))
+storm6$CROPDMG_t <- ifelse(storm6$CROPDMGEXP == "T", 1e1*storm6$CROPDMG,
+                           ifelse(storm6$CROPDMGEXP== "H", 1e2*storm6$CROPDMG,
+                                  ifelse(storm6$CROPDMGEXP == "K", 1e3*storm6$CROPDMG,
+                                         ifelse(storm6$CROPDMGEXP == "M", 1e6*storm6$CROPDMG,
+                                                ifelse(storm6$CROPDMGEXP == "B", 1e9*storm6$CROPDMG,
+                                                      ifelse(storm6$CROPDMGEXP == "O", 1*storm6$CROPDMG,0))))))
 
 
 ###############################################################################################
